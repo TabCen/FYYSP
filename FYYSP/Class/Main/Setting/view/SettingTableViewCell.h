598972a701569db5c257dd2feb494754021c517a
陈飞 <964267617@qq.com>
2016-11-21 18:01:16 +0800

    20161121提交
    MVC框架出成

---
+++ FYYSP/Class/Main/Setting/view/SettingTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SettingTableViewCell.h
+//  FYYSP
+//
+//  Created by  chenfei on 2016/11/21.
+//  Copyright © 2016年 chenfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingTableViewCell : UITableViewCell
+
+@end
